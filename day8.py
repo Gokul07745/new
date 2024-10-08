2dd2741e0ae66d7957f715598aafa6e155b4a290
Gokul07745 <gokul.07745@gmail.com>
2024-10-08 10:21:38 +0530

    created a new repository

---
+++ day8.py
@@ -0,0 +1,5 @@
+Fruits=["pomegranate","apple","orange","banana","grapes","cucumber"]
+Veggies=["ladiesfineger","potato","tomato","onion","capsicum","raddish"]
+dirty_foods=[Fruits,Veggies]
+print(dirty_foods)
+
